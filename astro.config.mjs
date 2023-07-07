44fd00c60d41886b6e7eb4e9f0a35b207fbb7da6
Diego Pagini <diego.pagini@neoris.com>
2023-07-07 11:40:00 +0200

    vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 /** @format */
-
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/serverless';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind()],
 	output: 'server',
+	adapter: vercel(),
 });
